71890f186a1a1da31b5ce701872a06004f79f9d3
Rafael Teno Dias <rfteno@gmail.com>
2024-11-09 23:47:54 -0400

    Alterações no demo em avalonia.

---
+++ src/Caramelo.MvvmApp.Demo.Core/ViewModels/DemoAppViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading.Tasks;
+using Caramelo.MvvmApp.ViewModel;
+using ReactiveUI.SourceGenerators;
+
+namespace Caramelo.MvvmApp.Demo.Core.ViewModels;
+
+public partial class DemoAppViewModel : AppViewModel
+{
+    public DemoAppViewModel(IServiceProvider service) : base(service)
+    {
+    }
+
+    [ReactiveCommand]
+    private async Task OpenDemoViewAsync()
+    {
+        await Navigation.GoToAsync<DemoViewModel>();
+    }
+}
---
+++ src/Caramelo.MvvmApp.WPF.Demo/Views/DemoAppView.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Caramelo.MvvmApp.Demo.Core.ViewModels;
+using Caramelo.MvvmApp.WPF.View;
+
+namespace Caramelo.MvvmApp.WPF.Demo.Views;
+
+public partial class DemoAppView : MvvmWindow<DemoAppViewModel>
+{
+    public DemoAppView()
+    {
+        InitializeComponent();
+    }
+}
